4d0038a22ca366b23cf2c1c1c38caf8f09af5175
Nirina1 <motuzkoip@gmail.com>
2018-07-06 16:54:30 +0200

    did refactoring of the classes
    the code for the user deletion does not work. function does not initialize the click on the button "delete"

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -1,13 +1,6 @@
 package ru.stqa.pft.addressbook;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
 
 public class GroupCreationTests extends TestBase {
 
