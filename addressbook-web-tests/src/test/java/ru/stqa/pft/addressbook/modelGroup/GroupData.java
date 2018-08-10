9318d3b17d5a7af89461e6af13be7539bbc5c857
Nirina1 <motuzkoip@gmail.com>
2018-08-10 18:07:43 +0200

    Created an option to compare Groups after creation, modification, and deletion
    elements are sorted before they are being compared

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/modelGroup/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/modelGroup/GroupData.java
@@ -1,14 +1,57 @@
 package ru.stqa.pft.addressbook.modelGroup;
 
+import java.util.Objects;
+
 public class GroupData {
+
+    private int id;
     private final String header;
     private final String footer;
     private final String name;
 
-    public GroupData(String header, String footer, String name) {
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "id='" + id +  '\'' +
+               ", name='" + name + '\'' +
+                '}';
+    }
+
+
+    public GroupData(int id, String header, String footer, String name) {
+        this.id = id;
         this.header = header;
         this.footer = footer;
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupData groupData = (GroupData) o;
+        return Objects.equals(name, groupData.name);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name);
+    }
+
+    public GroupData(String header, String footer, String name) {
+        this.id = Integer.MAX_VALUE;
+        this.header = header;
+        this.footer = footer;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getHeader() {
@@ -22,4 +65,5 @@ public class GroupData {
     public String getName() {
         return name;
     }
+
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/testGroup/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/testGroup/GroupCreationTests.java
@@ -4,6 +4,10 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.modelGroup.GroupData;
 
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
     @Test
@@ -11,10 +15,19 @@ public class GroupCreationTests extends TestBase {
 
 
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
-        app.getGroupHelper().createGroup(new GroupData("test1", null, "test1"));
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before + 1);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        GroupData group = new GroupData("test1", null, "test5");
+        app.getGroupHelper().createGroup(group);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() + 1);
+
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+        before.add(group);
+        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
+
     }
 
 }
