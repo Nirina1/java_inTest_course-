9c48d8989ca5a6f96305741f3b1506d66f0d6016
Nirina1 <motuzkoip@gmail.com>
2018-08-23 04:05:04 +0200

    Added logback.xml to create file for logs

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/testGroup/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/testGroup/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.testGroup;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.modelGroup.GroupData;
@@ -19,6 +21,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class GroupCreationTests extends TestBase {
+
+
+
     @DataProvider
     public Iterator<Object[]> validGroupsFromXml() throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")))) {
@@ -44,10 +49,12 @@ public class GroupCreationTests extends TestBase {
                 json += line;
                 line = reader.readLine();
             }
+
             Gson gson = new Gson();
             List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>() {
             }.getType());
             return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+
         }
     }
 
@@ -60,6 +67,7 @@ public class GroupCreationTests extends TestBase {
         Groups after = app.group().all();
         assertThat(after, equalTo(
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+
     }
 
 
